16b619729ad42c226b2cf41fe860f1d04f77f03a
epartan <elizabeth.partan@gmail.com>
2025-09-11 14:23:44 -0400

    Re-render lessons

--- download.sh
+++ download.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Set the URL and destination file name
-URL="https://www.dropbox.com/s/oz9yralwbtphw8u/data.zip?dl=1"
+# Salmon data only
+# URL="https://www.dropbox.com/s/oz9yralwbtphw8u/data.zip?dl=1"
+# Salmon data, annotation file, and all intermediate files for instructors
+URL="https://www.dropbox.com/scl/fo/87i5r9svrslnndkx3yy0x/AJuF2-q4cFgEBvqv9nOIh-w?rlkey=suf04w6fwq11ni2g4mx3ug65q&st=75c27zdk&dl=1"
 ZIPFILE="rnaseq_dge.zip"
 
 # Download the file
@@ -18,5 +21,5 @@ rm "$ZIPFILE"
 rm -rf __MACOSX
 rm -rf rnaseq_dge
 
-# AnnotationHub annotation file
-curl -L "https://github.com/hbctraining/DGE_workshop_salmon/raw/master/data/tx2gene_grch38_ens94.txt" -o "data/tx2gene_grch38_ens94.txt"
+# AnnotationHub annotation file (also included in above link)
+# curl -L "https://github.com/hbctraining/DGE_workshop_salmon/raw/master/data/tx2gene_grch38_ens94.txt" -o "data/tx2gene_grch38_ens94.txt"
