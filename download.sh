5ef330b329ec3c0464a9469e8601ff45a9232940
Gammerdinger <wjgdinger@gmail.com>
2025-11-13 16:44:05 -0500

    Re-rendered all lessons

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 # Set the URL and destination file name
 # Salmon data only
-# URL="https://www.dropbox.com/s/oz9yralwbtphw8u/data.zip?dl=1"
+# URL="https://www.dropbox.com/scl/fi/t2oirdy6qm0poy3uix5l6/data.zip?rlkey=xy39cwhjxectjp2y3txktcath&st=5i94dte0&dl=1"
 # Salmon data, annotation file, and all intermediate files for instructors
 URL="https://www.dropbox.com/scl/fo/87i5r9svrslnndkx3yy0x/AJuF2-q4cFgEBvqv9nOIh-w?rlkey=suf04w6fwq11ni2g4mx3ug65q&st=75c27zdk&dl=1"
 ZIPFILE="rnaseq_dge.zip"
@@ -14,7 +14,7 @@ curl -L "$URL" -o "$ZIPFILE"
 unzip "$ZIPFILE"
 
 # Move data folder
-mv rnaseq_dge/data . 
+mv rnaseq_dge/data lessons/
 
 # Remove the zip file after extraction
 rm "$ZIPFILE"
@@ -22,4 +22,4 @@ rm -rf __MACOSX
 rm -rf rnaseq_dge
 
 # AnnotationHub annotation file (also included in above link)
-# curl -L "https://github.com/hbctraining/DGE_workshop_salmon/raw/master/data/tx2gene_grch38_ens94.txt" -o "data/tx2gene_grch38_ens94.txt"
+# curl -L "https://www.dropbox.com/scl/fi/8qk2udv283l624ch6w3is/tx2gene_grch38_ens94.txt?rlkey=v47rdu1z11mlfvtvj1nzw2zy4&st=36yoijs1&dl=1" -o "lessons/data/tx2gene_grch38_ens94.txt"
